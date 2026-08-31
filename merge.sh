c2e1a0ac3840eec971799ba0046ad3b960460bb8
Jannes Höke <mail@jhoeke.de>
2026-08-31 11:50:03 +0200

    🐛 Fix generation of missing large tiles

--- merge.sh
+++ merge.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+# Some tiles are larger than the default 500kb limit for tile-join.
 ./../tippecanoe/tile-join \
+    --no-tile-size-limit \
     -o ./tilesets/nrw-v3.mbtiles \
     ./tilesets/coastline.mbtiles \
     ./tilesets/europe.mbtiles \
